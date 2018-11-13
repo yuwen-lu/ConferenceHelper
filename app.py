924159dd69d108245bed55b74dcad7e2322888cb
yuwen-lu <wayne.lu1998@gmail.com>
2018-11-13 23:30:51 +0800

    basic layout and navbar

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/countries')
+def countries():
+    country_list = [
+        "China",
+        "Germany",
+        "France",
+        "USA",
+        "UK",
+        "Russia",
+    ]
+    return render_template('country.html', country_list=country_list)
+
+
+@app.route('/timer')
+def timer():
+    time = 300
+    return render_template('timer.html', time=time)
+
+
+@app.route('/timer/<int:num>s')
+@app.route('/timer/<int:num>')
+def customizeTimer(num):
+    return render_template('timer.html', time=num)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
